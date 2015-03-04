15f42e717ed4a02fd6e20f27457c5ef2b66ab8b1
hisyamidris <akmal.idris@colorado.edu>
2015-03-04 11:04:06 -0700

    hotkey fix

---
+++ patches/patch_00219/data/UserKeys_s.lua
@@ -0,0 +1,12 @@
+USERKEY_1 = {
+	[0] = { EXE = "Hotkey 1-1", KEY1 = 81 },
+	[1] = { EXE = "Hotkey 1-2", KEY1 = 87 },
+	[2] = { EXE = "Hotkey 1-3", KEY1 = 69 },
+	[3] = { EXE = "Hotkey 1-4", KEY1 = 82 },
+	[4] = { EXE = "Hotkey 1-5", KEY1 = 70 },
+	[5] = { EXE = "Hotkey 1-6", KEY1 = 49 },
+	[6] = { EXE = "Hotkey 1-7", KEY1 = 50 },
+	[7] = { EXE = "Hotkey 1-8", KEY1 = 51 },
+	[8] = { EXE = "Hotkey 1-9", KEY1 = 52 },
+}
+
